651058f52f7dce145248e8810194b77a8a99d263
Joseph <mcal5071@gmail.com>
2020-09-28 11:23:23 +1300

    Modify for static variable

--- resistor-color/ResistorColor.cs
+++ resistor-color/ResistorColor.cs
@@ -3,11 +3,12 @@ using System;
 
 public static class ResistorColor
 {
+    public static string[] colorCode =  {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+
     public static int ColorCode(string color) => Array.IndexOf(Colors(), color);
 
     public static string[] Colors()
     {
-        string[] colorCode =  {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
         return colorCode;
     }
 }
