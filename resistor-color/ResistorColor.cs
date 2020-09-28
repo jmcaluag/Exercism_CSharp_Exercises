cb7448545a09c0d7f0000209cfe70d19b1a7169c
Joseph <mcal5071@gmail.com>
2020-09-29 10:25:57 +1300

    Modify for more concise and reduce resource usage

--- resistor-color/ResistorColor.cs
+++ resistor-color/ResistorColor.cs
@@ -3,12 +3,16 @@ using System;
 
 public static class ResistorColor
 {
-    public static string[] colorCode =  {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+    // This is a more concise way of writing this program without having to recreate the colorCode array every time it is run.
+    // Uncomment Colors() method and change IndexOf parameter to run for unit tests.
+    private static string[] colorCode =  {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
 
-    public static int ColorCode(string color) => Array.IndexOf(Colors(), color);
+    public static int ColorCode(string color) => Array.IndexOf(colorCode, color); // Change to Colors() to use the method.
 
-    public static string[] Colors()
-    {
-        return colorCode;
-    }
+    // Use to fulfill unit tests.
+
+    // public static string[] Colors()
+    // {
+    //     return colorCode;
+    // }
 }
