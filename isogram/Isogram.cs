2db421f15647a4109777254623416f8f0939692e
Joseph <mcal5071@gmail.com>
2020-09-28 15:05:28 +1300

    Complete Isogram.cs

--- isogram/Isogram.cs
+++ isogram/Isogram.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class Isogram
 {
     public static bool IsIsogram(string word)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<char> letters = new List<char>();
+
+        foreach (char character in word.ToLower())
+        {
+            // ASCII lowercase alphabet from 97 to 122
+            // ASCII value 32 and 45 are space and dash, respectively.
+            if(character > 96 && character < 123 && !letters.Contains(character) || (character == 32 || character == 45))
+            {
+                letters.Add(character);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
